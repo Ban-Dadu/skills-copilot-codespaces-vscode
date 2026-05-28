753d1bab25247ce6e347f5d596784868331dfd40
Ban Dadu <db6026010110@camtech.edu.kh>
2026-05-28 12:02:28 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -3,13 +3,19 @@ function skillsMember(){
         name: 'skills-member',
         template: `
             <div class="skills-member">
-                <h3>{{ name }}</h3>
-                <p>{{ description }}</p>
+                <div class="skills-member__avatar">
+                    <img :src="avatar" alt="Avatar">
+                </div>
+                <div class="skills-member__info">
+                    <h3 class="skills-member__name">{{ name }}</h3>
+                    <p class="skills-member__role">{{ role }}</p>
+                </div>
             </div>
         `,
         props: {
+            avatar: String,
             name: String,
-            description: String
-        }   
+            role: String
+        }
     }
 }
